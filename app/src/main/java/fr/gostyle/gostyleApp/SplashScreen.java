0422eef448e203d29eb90e7b0b238736ddbd3d2f
dodjionesime <dodjionesime@gmail.com>
2020-03-12 15:57:37 +0100

    ajout-introslider-splashscreen
    Création d'un intro slider et d'un splashscreen au démarrage de l'application

---
+++ app/src/main/java/fr/gostyle/gostyleApp/SplashScreen.java
@@ -0,0 +1,30 @@
+package fr.gostyle.gostyleApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                launchMainScreen();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+    // Launch Main Screen
+    private void launchMainScreen() {
+        startActivity(new Intent(SplashScreen.this, MainActivity.class));
+        finish();
+    }
+}
